56118fa16f34fcace5eaf43286a9659d0770f9b2
Thomas Marschall <thomasmarschall@mac.com>
2023-01-09 16:40:11 -0600

    Initial Commit

---
+++ KvoLab2023/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  KvoLab2023
+//
+//  Created by Thomas Marschall on 1/9/23.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+  @IBOutlet var window: NSWindow!
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
